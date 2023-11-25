abe45a12371227d297878f7f62ad6e45aa599454
lara <larapauser@mailbox.org>
2023-11-25 20:01:26 +0100

    make use of the advent-of-code-data package
    instead of saving the input as a text file beforehand, download it with help of the advent-of-code-data package
    
    this makes the 'inputs' folder obsolete

--- 2022/solutions/01_maxCalories.py
+++ 2022/solutions/01_maxCalories.py
@@ -1,24 +1,25 @@
-# open the text file input
-inventory = open('input','r')
-# read contents into a list inventoryLines
-inventoryLines = inventory.readlines()
-# close the file again
-inventory.close()
+# import the advent of code puzzle
+from aocd.models import Puzzle
+puzzle = Puzzle(year=2022, day=1)
+# open the puzzle input data
+inventory = puzzle.input_data
+# split the string into a list inventoryLines
+inventoryLines = inventory.split('\n')
 # create a nested list elfInventory in which each list corresponds to one elf's inventory entries
 elfInventory = [[]]
 elfIndex = 0
 # in inventoryLines, an empty line (just containing '\n') is the delimiter where another elf's entry begins
 for inventoryLine in inventoryLines:
-    if inventoryLine == '\n':
+    if inventoryLine == '':
         elfIndex += 1
         elfInventory.append([])
     else:
-        elfInventory[elfIndex].append(int(inventoryLine.strip('\n')))
+        elfInventory[elfIndex].append(int(inventoryLine))
 # calculate the sum total of each elf's calories, save in the list ElfCaloriesSum
 elfCaloriesSum = []
 for elfCalories in elfInventory:
     elfCaloriesSum.append(sum(elfCalories))
 print(elfCaloriesSum)
-# get the the biggest entry in elfCaloriesSum, save in the variable maxElfCalories
+# get the biggest entry in elfCaloriesSum, save in the variable maxElfCalories
 maxElfCalories = max(elfCaloriesSum)
-print ('most calories carried by one elf is', maxElfCalories)
+print('most calories carried by one elf is', maxElfCalories)
