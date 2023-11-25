e8c7b25fff8c71edc75e6e01dd80b7d0b22bdc4e
lara <larapauser@mailbox.org>
2023-11-25 20:24:44 +0100

    add basic structure
    add functions for parsing the input and solving the first part of the puzzle

--- 2022/solutions/01_maxCalories.py
+++ 2022/solutions/01_maxCalories.py
@@ -1,25 +1,34 @@
 # import the advent of code puzzle
 from aocd.models import Puzzle
-puzzle = Puzzle(year=2022, day=1)
-# open the puzzle input data
-inventory = puzzle.input_data
-# split the string into a list inventoryLines
-inventoryLines = inventory.split('\n')
-# create a nested list elfInventory in which each list corresponds to one elf's inventory entries
-elfInventory = [[]]
-elfIndex = 0
-# in inventoryLines, an empty line (just containing '\n') is the delimiter where another elf's entry begins
-for inventoryLine in inventoryLines:
-    if inventoryLine == '':
-        elfIndex += 1
-        elfInventory.append([])
-    else:
-        elfInventory[elfIndex].append(int(inventoryLine))
-# calculate the sum total of each elf's calories, save in the list ElfCaloriesSum
-elfCaloriesSum = []
-for elfCalories in elfInventory:
-    elfCaloriesSum.append(sum(elfCalories))
-print(elfCaloriesSum)
-# get the biggest entry in elfCaloriesSum, save in the variable maxElfCalories
-maxElfCalories = max(elfCaloriesSum)
-print('most calories carried by one elf is', maxElfCalories)
+
+# parse the puzzle input data
+def parse(puzzle_input) :
+    # split the string into a list inventoryLines
+    lines=puzzle_input.split('\n')
+    return lines
+
+def part1(numbers):
+    # create a nested list elfInventory in which each list corresponds to one elf's inventory entries
+    elfInventory = [[]]
+    elfIndex = 0
+    # in inventoryLines, an empty line (just containing '\n') is the delimiter where another elf's entry begins
+    for number in numbers:
+        if number == '':
+            elfIndex += 1
+            elfInventory.append([])
+        else:
+            elfInventory[elfIndex].append(int(number))
+    # calculate the sum total of each elf's calories, save in the list ElfCaloriesSum
+    elfCaloriesSum = []
+    for elfCalories in elfInventory:
+        elfCaloriesSum.append(sum(elfCalories))
+    print(elfCaloriesSum)
+    # get the biggest entry in elfCaloriesSum, save in the variable maxElfCalories
+    maxElfCalories = max(elfCaloriesSum)
+    return(maxElfCalories)
+
+if __name__ == '__main__':
+    puzzle = Puzzle(year=2022, day=1)
+    puzzle_input = puzzle.input_data
+    lines = parse(puzzle_input)
+    print (part1(lines))
