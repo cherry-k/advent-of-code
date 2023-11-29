e0d9f0b4a29fc55a1a3ef44157c3b05ca9a5a3cb
lara <larapauser@mailbox.org>
2023-11-29 20:52:45 +0100

    implement part 2 of today's puzzle

--- 2022/solutions/maxCalories.py
+++ 2022/solutions/maxCalories.py
@@ -12,8 +12,17 @@ def part1(numbers):
     maxElfCalories = max(elfCaloriesSum)
     return(maxElfCalories)
 
+def part2(numbers):
+    # calculate the sum total of each elf's calories, save in the list ElfCaloriesSum
+    elfCaloriesSum = [sum(e) for e in numbers]
+    # sort the list
+    elfCaloriesSum.sort(reverse=True)
+    return sum(elfCaloriesSum[0:3])
+
+
 if __name__ == '__main__':
     puzzle = Puzzle(year=2022, day=1)
     puzzle_input = puzzle.input_data
     lines = parse(puzzle_input)
     print(part1(lines))
+    print(part2(lines))
--- 2022/solutions/maxCalories_test.py
+++ 2022/solutions/maxCalories_test.py
@@ -16,8 +16,6 @@ def test_parse_example1(example1):
 def test_part1_example1(example1):
     assert maxCalories.part1(example1) == 24000
 
-@pytest.mark.skip(reason="Not implemented")
-
 def test_part2_example1(example1):
     """Test part 2 on example input."""
-    assert maxCalories.part2(example1) == ...
+    assert maxCalories.part2(example1) == 45000
