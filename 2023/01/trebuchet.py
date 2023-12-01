29f67b9eb7fcb50d1f6a7123340a9e89fda2abf3
lara <larapauser@mailbox.org>
2023-12-01 15:08:58 +0100

    implement day 1, part 2

--- 2023/01/trebuchet.py
+++ 2023/01/trebuchet.py
@@ -1,13 +1,40 @@
 # import the advent of code puzzle
 from aocd.models import Puzzle
+import re
 
 # parse the puzzle input data
 def parse(puzzle_input) :
     # split the string
     return puzzle_input.split()
 
+# replace first occurrence of 'one' with '1' etc
+def find_first_number(string):
+    numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    # check if string contains the number, if yes, return True, otherwise return False
+    pat = re.compile('|'.join([re.escape(s) for s in numbers]))
+    match = pat.search(string)
+    if match is None:
+        return -1
+    else:
+        #the corresponding number is the string modulo 9 because after 'nine' it starts with 1 again
+        number = numbers.index(match.group())%9 +1
+        return str(number)
+
+def find_last_number(string):
+    numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    # check if string contains the number, if yes, return True, otherwise return False
+    pat = re.compile('|'.join([re.escape(s[::-1]) for s in numbers]))
+    match = pat.search(string[::-1])
+    if match is None:
+        return -1
+    else:
+        #the corresponding number is the string modulo 9 because after 'nine' it starts with 1 again
+        number = numbers.index(match.group()[::-1])%9+1
+        return str(number)
+
 
  # combine the first digit and the last digit in each string to get the calibration values
+ # return the sum of all calibration values
 def part1(strings):
     # for each string, find the first and last digit
     digit_strings=[]
@@ -18,14 +45,23 @@ def part1(strings):
         digit_strings.append(int(digit_string))
     return sum(digit_strings)
 
-def part2(numbers):
-    # calculate the sum total of each elf's calories, save in the list ElfCaloriesSum
-    return numbers
+ # combine the first digit and the last digit in each string to get the calibration values
+ # return the sum of all calibration values
+ # digits spelled as words ('one', 'two' etc.) also count
+def part2(strings):
+    # first, replace the digits spelled as words with digits
+    digit_strings = []
+    for string in strings:
+        digit_string=''
+        digit_string += find_first_number(string)
+        digit_string += find_last_number(string)
+        digit_strings.append(int(digit_string))
+    return sum(digit_strings)
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=2023, day=1)
     puzzle_input = parse(puzzle.input_data)
-    sumCalibrationValues = part1(puzzle_input)
+    sumCalibrationValues = part2(puzzle_input)
     print(sumCalibrationValues)
 
--- 2023/01/trebuchet_test.py
+++ 2023/01/trebuchet_test.py
@@ -9,14 +9,20 @@ def example1():
     puzzle_input = (PUZZLE_DIR/'example.txt').read_text().strip()
     return trebuchet.parse(puzzle_input)
 
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR/'example2.txt').read_text().strip()
+    return trebuchet.parse(puzzle_input)
+
 def test_parse_example1(example1):
     assert example1 == ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
+
+def test_parse_example2(example2):
+    assert example2 == ['two1nine', 'eightwothree', 'abcone2threexyz', 'xtwone3four', '4nineeightseven2', 'zoneight234', '7pqrstsixteen']
 
 def test_part1_example1(example1):
     assert trebuchet.part1(example1) == 142
 
-@pytest.mark.skip(reason="Not implemented")
-
-def test_part2_example1(example1):
+def test_part2_example1(example2):
     """Test part 2 on example input."""
-    assert trebuchet.part2(example1) == 45000
+    assert trebuchet.part2(example2) == 281
