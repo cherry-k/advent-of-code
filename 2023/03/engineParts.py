99a2e381e6917e55e22b2fdff98f3f3a59010d44
lara <larapauser@mailbox.org>
2023-12-03 14:44:47 +0100

    day 3, part 1

--- 2023/03/engineParts.py
+++ 2023/03/engineParts.py
@@ -1,11 +1,32 @@
 # import the advent of code puzzle
 from aocd.models import Puzzle
+import re
 
-# parse the puzzle input data
-def parse(puzzle_input) :
-    ...
-def part1(numbers):
-    ...
+
+def parse(puzzle_input):
+    return puzzle_input.split('\n')
+
+
+def has_special_characters(string):
+    pat = re.compile('[@_!#$%^&*()<>=?/\|}{~:+-]')
+    if pat.search(string):
+        return True
+    else:
+        return False
+
+def part1(lines):
+    partNumbers = []
+    for row, line in enumerate(lines):
+        matches = re.finditer(r'\d+', line)
+        for match in matches:
+            number = match.group()
+            s_rows = lines[max(row-1,0):min(row+2, len(lines))]
+            searchstring = ''
+            for s_row in s_rows:
+                searchstring += s_row[max(match.start()-1,0):min(match.end()+1, len(line))]
+            if has_special_characters(searchstring):
+                partNumbers.append(int(number))
+    return sum(partNumbers)
 
 def part2(numbers):
     ...
@@ -14,6 +35,6 @@ def part2(numbers):
 if __name__ == '__main__':
     puzzle = Puzzle(year=2023, day=3)
     puzzle_input = puzzle.input_data
-    # lines = parse(puzzle_input)
-    # print(part1(lines))
+    lines = parse(puzzle_input)
+    print(part1(lines))
     # print(part2(lines))
--- 2023/03/engineParts_test.py
+++ 2023/03/engineParts_test.py
@@ -11,11 +11,10 @@ def example1():
 
 @pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1):
-    assert example1 == [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
+    assert example1 == []
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
-    assert engineParts.part1(example1) == 24000
+    assert engineParts.part1(example1) == 4
 
 @pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1):
