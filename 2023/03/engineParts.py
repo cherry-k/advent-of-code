7ce278c83ec96a0dfbbcb1246f0759e7e8a11c78
lara <larapauser@mailbox.org>
2023-12-03 10:37:27 +0100

    start day 3

---
+++ 2023/03/engineParts.py
@@ -0,0 +1,19 @@
+# import the advent of code puzzle
+from aocd.models import Puzzle
+
+# parse the puzzle input data
+def parse(puzzle_input) :
+    ...
+def part1(numbers):
+    ...
+
+def part2(numbers):
+    ...
+
+
+if __name__ == '__main__':
+    puzzle = Puzzle(year=2023, day=3)
+    puzzle_input = puzzle.input_data
+    # lines = parse(puzzle_input)
+    # print(part1(lines))
+    # print(part2(lines))
