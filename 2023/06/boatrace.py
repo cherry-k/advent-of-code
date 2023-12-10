7feac5d7c986302632fe45734c23198376704e39
lara <larapauser@mailbox.org>
2023-12-10 13:07:59 +0100

    day 6, part 2

--- 2023/06/boatrace.py
+++ 2023/06/boatrace.py
@@ -10,30 +10,42 @@ DAY = 6
 def parse(puzzle_input):
     return [[int(member) for member in line.split(':')[1].split()] for line in puzzle_input.split('\n')]
 
+def parse2(puzzle_input):
+    return [int(line.split(':')[1].replace(" ", "")) for line in puzzle_input.split('\n')]
+
 def charge(milliseconds):
     speed = 0
     speed += milliseconds
     return speed
 
+def get_charging_times(race_time, distance):
+    charging_time_1 = (race_time + math.sqrt(math.pow(race_time, 2) - 4*distance))/2
+    charging_time_2 = (race_time - math.sqrt(math.pow(race_time, 2) - 4*distance))/2
+
+    charging_time_1 = math.floor(charging_time_1) if not charging_time_1.is_integer() else charging_time_1-1
+    charging_time_2 = math.ceil(charging_time_2) if not charging_time_2.is_integer() else charging_time_2+1
+
+    return 1 + charging_time_1 - charging_time_2
 def part1(lines):
     times = lines[0]
     distances = lines[1]
     number_of_ways = []
-    for time, distance in zip(times, distances):
-        charging_times = range(1, time-1, 1)
-        my_distances = [(time - charging_time) * charging_time for charging_time in charging_times]
-        number_of_ways.append(sum(i > distance for i in my_distances))
+    for race_time, distance in zip(times, distances):
+        ways = get_charging_times(race_time, distance)
+        number_of_ways.append(ways)
     return math.prod(number_of_ways)
+    #return number_of_ways
 
 
 def part2(lines):
-    ...
-    return 0
+    race_time = lines[0]
+    distance = lines[1]
+    return get_charging_times(race_time, distance)
 
 
 if __name__ == "__main__":
     puzzle = Puzzle(year=YEAR, day=DAY)
     puzzle_input = puzzle.input_data
-    lines = parse(puzzle_input)
-    print(part1(lines))
-    print(part2(lines))
+    #lines = parse(puzzle_input)
+    #print(part1(lines))
+    print(part2(parse2(puzzle_input)))
--- 2023/06/boatrace_test.py
+++ 2023/06/boatrace_test.py
@@ -9,13 +9,21 @@ def example1():
     puzzle_input = (PUZZLE_DIR/'example.txt').read_text().strip()
     return boatrace.parse(puzzle_input)
 
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR/'example.txt').read_text().strip()
+    return boatrace.parse2(puzzle_input)
+
+@pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1):
     assert example1 == [[7, 15, 30], [9, 40, 200]]
+
+def test_parse_example2(example2):
+    assert example2 == [71530, 940200]
 
 def test_part1_example1(example1):
     assert boatrace.part1(example1) == 288
 
-@pytest.mark.skip(reason="Not implemented")
-def test_part2_example1(example1):
+def test_part2_example1(example2):
     """Test part 2 on example input."""
-    assert boatrace.part2(example1) == ...
+    assert boatrace.part2(example2) == 71503
