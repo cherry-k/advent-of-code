eb9a2c3e9bccec1a2e10c8f71445aa6472edde77
lara <larapauser@mailbox.org>
2023-12-21 01:00:40 +0100

    day 7 part 1

--- 2023/07/camelcards.py
+++ 2023/07/camelcards.py
@@ -1,108 +1,135 @@
-#from aocd.models import Puzzle
+from operator import itemgetter
+
+from aocd.models import Puzzle
 import pathlib
+from collections import defaultdict
+
 YEAR = 2023
-DAY = 3
+DAY = 7
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
 CARD_LABELS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'T', 'J', 'Q', 'K', 'A']
+CARD_VALUES = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'T':11, 'J':12, 'Q':13, 'K':14, 'A':15}
 
 
 def parse(puzzle_input):
     return [line.split() for line in puzzle_input.split('\n')]
 
-def sort_by_label(hands, labelcounts):
-    sorted_hands = []
-    while len(hands) > 0:
-        if len(hands) == 1:
-            sorted_hands.append(hands[0])
-            hands.pop(0)
-            labelcounts.pop(0)
+def check_full_house(hand):
+    values = [i for i in hand]
+    value_counts = defaultdict(lambda:0)
+    for v in values:
+        value_counts[v]+=1
+    if sorted(value_counts.values()) == [2,3]:
+        return True
+    return False
+
+def check_two_pairs(hand):
+    values = [i for i in hand]
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+    if sorted(value_counts.values()) == [1,2,2]:
+        return True
+    return False
+
+def count_equals(hand):
+    values = [i for i in hand]
+    value_counts = defaultdict(lambda:0)
+    for v in values:
+        value_counts[v]+=1
+    return max(value_counts.values())
+
+
+def check_hand_type(hand):
+    # hand values:
+    # five of a kind: 7
+    # four of a kind: 6
+    # full house: 5
+    # three of a kind: 4
+    # two pairs: 3
+    # one pair: 2
+    # high card: 1
+    if check_full_house(hand):
+        return 5
+    if check_two_pairs(hand):
+        return 3
+    else:
+        equals = count_equals(hand)
+        if equals > 3:
+            return equals + 2
+        if equals > 2:
+            return equals + 1
         else:
-            #add the indices and check for highest sum
-            sum_indices = [sum([(i+1)*num for i, num in enumerate(labelcount) if num >0]) for labelcount in labelcounts]
-            index_hand = sum_indices.index(max(sum_indices))
-            sorted_hands.append(hands[index_hand])
-            hands.pop(index_hand)
-            labelcounts.pop(index_hand)
-            print(hands)
-    return sorted_hands
+            return equals
 
-def sort_by_type(hands, winning_hands, of_a_kind):
-    # first, find out how often each card type appears
-    if not hands:
-        return winning_hands
-    label_occurrence_per_hand = [[] for k in hands]
-    # how often does each card type occur per hand?
-    for card_label in CARD_LABELS:
-        for index, line in enumerate(hands):
-            label_occurrence_per_hand[index].append(line[0].count(card_label))
-    count_of_a_kind = [hand.count(of_a_kind) for hand in label_occurrence_per_hand]
-    if count_of_a_kind == [0 for k in hands]:
-        of_a_kind -= 1
-        sort_by_type(hands, winning_hands, of_a_kind)
-    labelcounts_with_type = []
-    if of_a_kind == 3:
-        hands_with_type = []
-        labelcounts_with_type = []
-    # if there is three equals, we have to check for full house
-        for index_hand, count in enumerate(count_of_a_kind):
-            count_of_2 = [hand.count(2) for hand in label_occurrence_per_hand]
-            if count_of_2[index_hand] == 1 and count == 1:
-                print("Hand", hands[index_hand], "has a full house")
-                #add it to the hands to be sorted in next step
-                hands_with_type.append(hands[index_hand])
-                labelcounts_with_type.append(label_occurrence_per_hand[index_hand])
-                #remove it from the hands
-                hands.pop(index_hand)
-        if hands_with_type:
-            sorted_hands = sort_by_label(hands_with_type, labelcounts_with_type)
-            winning_hands.extend(sorted_hands)
-            sort_by_type(hands, winning_hands, of_a_kind)
-    if of_a_kind == 2:
-        hands_with_type = []
-        labelcounts_with_type = []
-    # if there is two of a kind, we have to check for two pairs
-        for index_hand, count in enumerate(count_of_a_kind):
-            if count == 2:
-                print("Hand", hands[index_hand], "has two pairs")
-                hands_with_type.append(hands[index_hand])
-                labelcounts_with_type.append(label_occurrence_per_hand[index_hand])
-                #remove it from the hands
-        if hands_with_type:
-            sorted_hands = sort_by_label(hands_with_type, labelcounts_with_type)
-            winning_hands.extend(sorted_hands)
-            for hand in sorted_hands:
-                hands.remove(hand)
-            sort_by_type(hands, winning_hands, of_a_kind)
-    hands_with_type = []
-    labelcounts_with_type = []
+def check_card_values(last_hand, this_hand):
+    last_values = [CARD_VALUES[i] for i in last_hand]
+    this_values = [CARD_VALUES[i] for i in this_hand]
+    for i in range(0,len(last_values)):
+        if last_values[i] > this_values[i]:
+            return True
+        if last_values[i] < this_values[i]:
+            return False
+    return False
 
-    for index_hand, count in enumerate(count_of_a_kind):
-        if count > 0 and hands[index_hand] not in winning_hands:
-            print("Hand", hands[index_hand], "has ", of_a_kind, " of a kind")
-            hands_with_type.append(hands[index_hand])
-            labelcounts_with_type.append(label_occurrence_per_hand[index_hand])
-    if hands_with_type:
-        sorted_hands = sort_by_label(hands_with_type, labelcounts_with_type)
-        winning_hands.extend(sorted_hands)
-        of_a_kind -= 1
-        for hand in sorted_hands:
-            hands.remove(hand)
-        sort_by_type(hands, winning_hands, of_a_kind)
+def check_greater(last_hand, this_hand):
+    hand_types = defaultdict(lambda:0)
+    hand_types[last_hand] = check_hand_type(last_hand)
+    hand_types[this_hand] = check_hand_type(this_hand)
+    card_values = defaultdict(lambda: 0)
+    if hand_types[last_hand] == hand_types[this_hand]:
+        if check_card_values(last_hand, this_hand):
+            return True
+        else:
+            return False
+    else:
+        if hand_types[last_hand] > hand_types[this_hand]:
+            return True
+        else:
+            return False
 
-def part1(lines):
-    winning_hands = []
-    winning_hands = sort_by_type(lines, winning_hands, 5)
-    return 0
+def swapPositions(lis, pos1, pos2):
+    temp=lis[pos1]
+    lis[pos1]=lis[pos2]
+    lis[pos2]=temp
+    return lis
+def part1(hands):
+    # compare 2 hands
+    n = len(hands)
+    swapped = True
+    while swapped:
+        swapped = False
+        for num in range(1, n):
+            last_hand = hands[num-1][0]
+            this_hand = hands[num][0]
+            if check_greater(last_hand, this_hand):
+                hands = swapPositions(hands, num-1, num)
+                swapped = True
+        n = n-1
+    winnings = 0
 
+    # f = open(PUZZLE_DIR / 'example2.txt', "a")
+    # glue = ' '
+    # for num,hand in enumerate(hands):
+    #     cards = [[i, CARD_VALUES[i]] for i in hand[0]]
+    #     cards = sorted(cards, key=itemgetter(1), reverse=True)
+    #     str = ''
+    #     hands[num][0] = str.join([i[0] for i in cards])
+
+    # hands = [glue.join(hand) for hand in hands]
+    # f.writelines(line + '\n' for line in hands)
+    # f.close()
+    for hand in hands:
+        winnings += (hands.index(hand)+1)*int(hand[1])
+    return winnings
 def part2(lines):
     ...
     return 0
 
 if __name__ == "__main__":
-    #puzzle = Puzzle(year=YEAR, day=DAY)
-    #puzzle_input = puzzle.input_data
-    puzzle_input = (PUZZLE_DIR / 'example.txt').read_text().strip()
+    puzzle = Puzzle(year=YEAR, day=DAY)
+    puzzle_input = puzzle.input_data
     lines = parse(puzzle_input)
     print(part1(lines))
     #print(part2(lines))
--- 2023/07/camelcards_test.py
+++ 2023/07/camelcards_test.py
@@ -14,7 +14,7 @@ def test_parse_example1(example1):
     assert example1 == [['32T3K', '765'], ['T55J5', '684'], ['KK677', '28'], ['KTJJT', '220'], ['QQQJA', '483']]
 
 def test_part1_example1(example1):
-    assert camelcards.part1(example1) == 0
+    assert camelcards.part1(example1) == 6440
 
 @pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1):
